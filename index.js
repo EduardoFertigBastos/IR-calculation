0a75dc877fa5d07227c5e13abefbc6c45b132c34
Eduardo Fertig Bastos <dudufbastos1@gmail.com>
2024-04-24 20:52:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 function calcIRPF(sallary) {
   if (sallary > 4664.68) {
-    return (sallary * 0.275) - 896
+    return (sallary * 0.275) - 896;
   }
 
   if (sallary > 3751.06) {
-    return (sallary * 0.225) - 662.77
+    return (sallary * 0.225) - 662.77;
   }
 
   if (sallary > 2826.66) {
-    return (sallary * 0.15) - 381.44
+    return (sallary * 0.15) - 381.44;
   }
 
   if (sallary > 2259.21) {
-    return (sallary * 0.075) - 196.44
+    return (sallary * 0.075) - 196.44;
   }
 
   return 0;
@@ -20,20 +20,20 @@ function calcIRPF(sallary) {
 
 function calcInss(sallary) {
   if (sallary > 7786.02) {
-    return (7786.02 * 0.14) - 181.18
+    return (7786.02 * 0.14) - 181.18;
   }
 
   if (sallary > 4000.03) {
-    return (sallary * 0.14) - 181.18
+    return (sallary * 0.14) - 181.18;
   }
 
   if (sallary > 2666.68) {
-    return (sallary * 0.12) - 101.18
+    return (sallary * 0.12) - 101.18;
   }
 
   if (sallary > 2666.68) {
-    return (sallary * 0.09) - 21.18
+    return (sallary * 0.09) - 21.18;
   }
 
-  return sallary * 0.075
+  return sallary * 0.075;
 }
