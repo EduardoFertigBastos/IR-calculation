03a2e6037492ed0bc3f3f20c948a9d6861b80a13
Eduardo Fertig Bastos <dudufbastos1@gmail.com>
2024-04-24 20:57:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,50 @@
-function calcIRPF(sallary) {
-  if (sallary > 4664.68) {
-    return (sallary * 0.275) - 896;
+function calcIRPF(salary) {
+  if (salary > 4664.68) {
+    return (salary * 0.275) - 896;
   }
 
-  if (sallary > 3751.06) {
-    return (sallary * 0.225) - 662.77;
+  if (salary > 3751.06) {
+    return (salary * 0.225) - 662.77;
   }
 
-  if (sallary > 2826.66) {
-    return (sallary * 0.15) - 381.44;
+  if (salary > 2826.66) {
+    return (salary * 0.15) - 381.44;
   }
 
-  if (sallary > 2259.21) {
-    return (sallary * 0.075) - 196.44;
+  if (salary > 2259.21) {
+    return (salary * 0.075) - 196.44;
   }
 
   return 0;
 }
 
-function calcFgts(sallary) {
-  return sallary * 0.08;
+function calcFgts(salary) {
+  return salary * 0.08;
 }
 
-function calcInss(sallary) {
-  if (sallary > 7786.02) {
+function calcInss(salary) {
+  if (salary > 7786.02) {
     return (7786.02 * 0.14) - 181.18;
   }
 
-  if (sallary > 4000.03) {
-    return (sallary * 0.14) - 181.18;
+  if (salary > 4000.03) {
+    return (salary * 0.14) - 181.18;
   }
 
-  if (sallary > 2666.68) {
-    return (sallary * 0.12) - 101.18;
+  if (salary > 2666.68) {
+    return (salary * 0.12) - 101.18;
   }
 
-  if (sallary > 2666.68) {
-    return (sallary * 0.09) - 21.18;
+  if (salary > 2666.68) {
+    return (salary * 0.09) - 21.18;
   }
 
-  return sallary * 0.075;
+  return salary * 0.075;
+}
+
+function yearlyCLTsalary(salary, vr) {
+  const fgts = calcFgts(salary);
+  const inss = calcInss(salary);
+  
+  const salaryDiscounted
 }
