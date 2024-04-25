cc9e4cd3b05f32a873bf08f28aeb581c78bff7a7
Eduardo Fertig Bastos <dudufbastos1@gmail.com>
2024-04-24 22:47:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -45,14 +45,33 @@ function calcInss(salary) {
 function yearlyCLTsalary(salary, vr = 0) {
   const fgts = calcFgts(salary);
   const inss = calcInss(salary);
-  
-  const salaryDiscounted = salary - inss;
+  const irpf = calcIRPF(salary - inss);
 
-  const irpf = calcIRPF(salaryDiscounted);
+  const monthSalary = salary + fgts + vr - irpf - inss;
 
-  const monthSalary = salary + fgts + vr - inss - irpf;
-  
   return monthSalary * 13.3
+}
+
+function yearlyPJSimple(salary) {
+  const employeeSalary = salary > 2000 ? 2000 : salary;
+  const inss = calcInss(employeeSalary);
+  const irpf = calcIRPF(employeeSalary - inss);
+  const irpj = salary * 0.15;
+
+  const monthSalary = salary - inss - irpj - irpf;
+   
+  return monthSalary * 12
+}
+
+function yearlyPJComplex(salary) {
+  const employeeSalary = salary * 0.28;
+  const inss = calcInss(employeeSalary);
+  const irpf = calcIRPF(employeeSalary - inss);
+  const irpj = salary * 0.06;
+
+  const monthSalary = salary - inss - irpj - irpf;
+   
+  return monthSalary * 12
 }
 
 const formatNumber = new Intl.NumberFormat('pt-BR', { 
@@ -62,6 +81,21 @@ const formatNumber = new Intl.NumberFormat('pt-BR', {
   maximumFractionDigits: 3 
 })
 
-const salary = 200;
-const clt = formatNumber.format(yearlyCLTsalary(salary));
-console.log({clt})
+for (let i = 0; i < 23; i++) {
+  const salary = 1000 * i;
+  const VR = 0;
+
+  const clt = yearlyCLTsalary(salary, VR);
+  const pj1 = yearlyPJSimple(salary);
+  const pj2 = yearlyPJComplex(salary);
+
+  console.log({
+    'Salary': salary,
+    'CLT': formatNumber.format(clt),
+    'PJ Simples': formatNumber.format(pj1),
+    'PJ Fator R': formatNumber.format(pj2),
+    'CLT > simples': (clt / pj1).toFixed(2),
+    'CLT > Fator R': (clt / pj2).toFixed(2),
+    'Simples > Fator R': (pj1 / pj2).toFixed(2)
+  }) 
+}
